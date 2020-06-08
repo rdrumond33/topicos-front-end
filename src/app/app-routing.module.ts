a1039dfe21daeda6dc3f73dd7c477fa6b1c2cd1c
Rodrigo Drumond de Paula <rodrigodru33@gmail.com>
2020-06-08 17:30:27 -0300

    blw

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,45 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
-  {path:'/pessoas',
-  loadChildren:()=>import('./pessoa/pessoa.module').then(p=>p.PessoaModule)
-}
+  {
+    path: '/anotacao',
+    loadChildren: () =>
+      import('./anotacao/anotacao.module').then((p) => p.AnotacaoModule),
+  }, {
+    path: '/grupo',
+    loadChildren: () =>
+      import('./grupo/grupo.module').then((p) => p.GrupoModule),
+  },
+  {
+    path: '/login',
+    loadChildren: () =>
+      import('./login/login.module').then((p) => p.LoginModule),
+  }, {
+    path: '/perfil',
+    loadChildren: () =>
+      import('./perfil/perfil.module').then((p) => p.PerfilModule),
+  }, {
+    path: '/perfilRecurso',
+    loadChildren: () =>
+      import('./perfilrecurso/perfilrecurso.module').then((p) => p.PerfilrecursoModule),
+  }, {
+    path: '/pessoas',
+    loadChildren: () =>
+      import('./pessoa/pessoa.module').then((p) => p.PessoaModule),
+  }, {
+    path: '/pessoasgrupo',
+    loadChildren: () =>
+      import('./pessoagrupo/pessoagrupo.module').then((p) => p.PessoagrupoModule),
+  },{
+    path: '/recurso',
+    loadChildren: () =>
+      import('./recurso/recurso.module').then((p) => p.RecursoModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
